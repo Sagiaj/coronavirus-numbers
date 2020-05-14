57f95a4be85d517a786c661197b873e87b9586e1
sagiaj <sagiajaj@gmail.com>
2020-05-14 21:48:41 +0300

    https and client only

--- components/CountriesMap.vue
+++ components/CountriesMap.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="map-wrap" style="height: 60vh; width: 100%;">
-    <no-ssr>
+    <client-only>
       <l-map :zoom="5" :center="center" :style="'z-index: 0;'">
         <l-tile-layer url="https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}"></l-tile-layer>
         <l-circle
@@ -14,7 +14,7 @@
           @click="openPopUp([circle.lat, circle.long], circle)"
         />
       </l-map>
-    </no-ssr>
+    </client-only>
     <v-dialog v-model="openDialog" color="red">
       <v-lazy v-model="calledCountry" transition="slide-y-transition" group>
         <transition name="slide-y-transition" mode="out-in">
--- utilities/api.ts
+++ utilities/api.ts
@@ -12,5 +12,5 @@ export const CoronaRoutes = {
   sortCountries: (sort: any[]) => `${base_api_url}/v2/countries?sort=${sort ? (Array.isArray(sort) ? sort.join(',') : sort) : "cases"}`,
   historical: (param: any) => `${base_api_url}/v2/historical/${param}`,
   country: (c: any) => `${base_api_url}/v2/countries/${c}`,
-  newsTimeline: () => `http://newsapi.org/v2/everything?q=coronavirus%20news&from=${getFormattedDate()}&sortBy=publishedAt&apiKey=73dd82c7c6064a38917f99ba04e41183`
+  newsTimeline: () => `https://newsapi.org/v2/everything?q=coronavirus%20news&from=${getFormattedDate()}&sortBy=publishedAt&apiKey=73dd82c7c6064a38917f99ba04e41183`
 };
